8e16143f39fbeb7a739b114363338179f75e8f74
tosyan3 <tosyanto3tttbb+bitbu@gmail.com>
2019-12-01 13:12:38 +0900

    add useState second button

--- src/App.js
+++ src/App.js
@@ -3,13 +3,17 @@ import './App.css';
 
 function App() {
   const [count, setCount] = useState(0);
-
+  const [second_count, setSecondCount] = useState(0);
   return (
-    <div className="App">
+  <div className="App">
       <header className="App-header">
         <p>You clicked {count} times</p>
         <button onClick={() => setCount(count + 1)}>
           Click me
+        </button>
+        <p>Yout clicked {second_count} times</p>
+        <button onClick={() => setSecondCount(second_count + 1)}>
+          Click me.Second button
         </button>
       </header>
     </div>
