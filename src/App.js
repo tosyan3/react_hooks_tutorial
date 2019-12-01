d0935dbfad41337670402808e09f5568dc79ce40
tosyan3 <tosyanto3tttbb+bitbu@gmail.com>
2019-12-01 19:39:34 +0900

    add second useState

--- src/App.js
+++ src/App.js
@@ -4,6 +4,13 @@ import './App.css';
 function App() {
   const [count, setCount] = useState(0);
   const [second_count, setSecondCount] = useState(0);
+  const [second_text, setCountText] = useState({text:"Help! Push Second Button"});
+
+  const onclickFunction = () => {
+    setSecondCount(second_count + 1);
+    setCountText({text:"Thank you for putting button!!!!"})
+  }
+
   return (
   <div className="App">
       <header className="App-header">
@@ -11,8 +18,11 @@ function App() {
         <button onClick={() => setCount(count + 1)}>
           Click me
         </button>
-        <p>Yout clicked {second_count} times</p>
-        <button onClick={() => setSecondCount(second_count + 1)}>
+        <p>
+          {second_text.text}.
+          Yout clicked {second_count} times
+        </p>
+        <button onClick={onclickFunction}>
           Click me.Second button
         </button>
       </header>
